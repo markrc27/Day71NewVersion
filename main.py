32fdaa22dcb4af51c1fd93161ad52fd68d653d0a
markrc27 <mark.r.cochrane@gmail.com>
2024-01-30 22:53:17 -0500

    Added env variable reference changes (lesson 547).

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -43,7 +43,8 @@ login_manager.init_app(app)
 # CONNECT TO DB
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'instance','posts.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI',
+                                                       'sqlite:///' + os.path.join(basedir, 'instance','posts.db'))
 
 db = SQLAlchemy()
 db.init_app(app)
@@ -296,4 +297,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False)
